33603e9ec59830daa7c31534c41a4b564212433a
zdenevuk <zdenko.vuk@.ijs.si>
2022-06-26 18:24:05 +0200

    still not getting the proper car's image

--- src/app/pages/single-car-display/single-car-display.component.ts
+++ src/app/pages/single-car-display/single-car-display.component.ts
@@ -12,6 +12,7 @@ import { Car } from "../../strpedova"
 export class SingleCarDisplayComponent implements OnInit {
   clickedSpan: number = 0
   responseCar: any = undefined
+  responseImage: string = ""
 
   constructor(private httpClient: HttpClient) { }
 
@@ -41,12 +42,24 @@ export class SingleCarDisplayComponent implements OnInit {
           val.carData[7][1],
           val.carData[8][1],
         )
+        this.responseImage = "http://localhost:3000/images/cars/toyota_celica.jpeg"
         setTimeout(() => {
           const el: HTMLElement | null = document.querySelector('#cardA')
           if (el) {
             el.classList.add('active')
           }
         }, 500)
+      },
+      error(msg) {
+        console.log('Error: ', msg);
+      }
+    })
+
+    this.getSingleCarImage().subscribe({
+      next: (val: any) => {
+        // console.log("🚀 ~ file: single-car-display.component.ts ~ line 58 ~ SingleCarDisplayComponent ~ this.getSingleCarImage ~ val", val)
+        // console.log(typeof(val))
+        this.responseImage = val
       },
       error(msg) {
         console.log('Error: ', msg);
@@ -68,14 +81,18 @@ export class SingleCarDisplayComponent implements OnInit {
   getSingleCar(): Observable<any> {
     return this.httpClient.get(`http://127.0.0.1:3000/cars/get-single-car/${this.clickedSpan}`, {}).pipe(
       // map(simulation => Simulation.deserialize(simulation)),
-      tap(res => console.log("res: ", res))
+      // tap(res => console.log("res: ", res))
     );
+  }
+
+  getSingleCarImage(): Observable<Blob> {
+    return this.httpClient.get(`http://127.0.0.1:3000/cars/get-single-car-image/${this.clickedSpan}`, { responseType: 'blob' })
   }
 
   getAllCars(): Observable<any> {
     return this.httpClient.get(`http://127.0.0.1:3000/cars/get-all-cars`, {}).pipe(
       // map(simulation => Simulation.deserialize(simulation)),
-      tap(res => console.log("res: ", res))
+      // tap(res => console.log("res: ", res))
     );
   }
 }
