196eaaf9955775fd89efebfdeb83d0f632ca1669
beaverDamDemo <zdenevi@gmail.com>
2023-02-28 14:32:36 +0100

    added a component

---
+++ src/app/myassistant/juliaiva/juliaiva/juliaiva.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juliaiva',
+  template: `
+    <p>
+      juliaiva works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class JuliaivaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/myassistant/myassistant.module.ts
+++ src/app/myassistant/myassistant.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyassistantRoutingModule } from './myassistant-routing.module';
+import { JuliaivaComponent } from './juliaiva/juliaiva/juliaiva.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JuliaivaComponent
+  ],
   imports: [
     CommonModule,
     MyassistantRoutingModule
