341cf2f0d1d3a76327af0e80c957a90eef052c43
beaverDamDemo <zdenevi@gmail.com>
2023-02-28 15:39:20 +0100

    update

--- src/app/myassistant/juliaiva/juliaiva/juliaiva.component.ts
+++ src/app/myassistant/juliaiva/juliaiva/juliaiva.component.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-juliaiva',
-  template: `
-    <p>
-      juliaiva works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>juliaiva works!</p> `,
+  styles: [],
 })
 export class JuliaivaComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const cartelletta = {
+      aspetto: 'tecnico' as string,
+      fatto: 'non si tratta' as string | number,
+    };
+    cartelletta.aspetto = cartelletta.aspetto.toUpperCase();
+    cartelletta['base'] = 'base';
+    console.log('⚛ ~ cartelletta:', cartelletta);
+    const colors = ['red', 'blue', 'green', 'orange', 'purple', 'pink'];
+    colors[2] = 'kramatorsk';
+    console.log('⚛ ~ colors:', colors);
   }
-
 }
