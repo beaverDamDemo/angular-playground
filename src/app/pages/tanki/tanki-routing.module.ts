d32e7219687882aabb73bf804df3c4ea9c46eeaf
beaverDamDemo <zdenevi@gmail.com>
2023-03-01 16:33:21 +0100

    update

--- src/app/pages/tanki/tanki-routing.module.ts
+++ src/app/pages/tanki/tanki-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeavyTankComponent } from './heavy-tank/heavy-tank.component';
+import { TankiComponent } from './tanki/tanki.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HeavyTankComponent,
+  },
+  {
+    path: 'tanki',
+    component: TankiComponent,
   },
 ];
 
