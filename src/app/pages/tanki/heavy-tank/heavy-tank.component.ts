260aa305f9ac891c17d5ff507cf8524d4eb41dcb
beaverDamDemo <zdenevi@gmail.com>
2023-03-02 09:30:06 +0100

    update

--- src/app/pages/tanki/heavy-tank/heavy-tank.component.ts
+++ src/app/pages/tanki/heavy-tank/heavy-tank.component.ts
@@ -2,14 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-tank',
-  templateUrl: './heavy-tank.component.html',
-  styleUrls: ['./heavy-tank.component.scss']
+  template: ` <div [innerText]="labelcek"></div> `,
+  styles: [],
 })
 export class HeavyTankComponent implements OnInit {
+  labelcek: string = 'I am heavy tank';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
