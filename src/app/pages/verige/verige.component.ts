32910fa4b2fa573bdbfa72180b728671b7413185
beaverDamDemo <zdenevi@gmail.com>
2023-03-02 15:54:02 +0100

    working on verige

--- src/app/pages/verige/verige.component.ts
+++ src/app/pages/verige/verige.component.ts
@@ -4,15 +4,36 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-verige',
   template: `
     <p>verige works!</p>
-    <!-- <nb-layout>
-      <nb-layout-header></nb-layout-header>
-      <nb-layout-footer></nb-layout-footer>
-      <nb-layout-column></nb-layout-column>
-      <nb-sidebar></nb-sidebar>
-    </nb-layout> -->
-    <button nbButton>button</button>
+    <button fullWidth="" nbButton status="success">button</button>
+    <div class="parent">
+      <div class="left">
+        <h5>left part</h5>
+        <ul>
+          <li>husky</li>
+          <li>rgchain</li>
+          <li>low-profile</li>
+          <li>normal-profile</li>
+          <li>full-skip</li>
+        </ul>
+      </div>
+      <div class="right">
+        <h5>right part</h5>
+      </div>
+    </div>
   `,
-  styles: [],
+  styles: [
+    `
+      div.parent {
+        background: lightblue;
+      }
+      div.left {
+        background: slategrey;
+      }
+      div.right {
+        background: azure;
+      }
+    `,
+  ],
 })
 export class VerigeComponent implements OnInit {
   constructor() {}
