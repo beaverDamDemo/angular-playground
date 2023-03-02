f98299005b27881ca16636d94584ba60286d3064
beaverDamDemo <zdenevi@gmail.com>
2023-03-02 12:19:33 +0100

    dodana slikca

--- src/app/pages/tanki/heavy-tank/ussr/kv1/kv1.component.ts
+++ src/app/pages/tanki/heavy-tank/ussr/kv1/kv1.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-kv1',
-  templateUrl: './kv1.component.html',
-  styleUrls: ['./kv1.component.scss'],
+  template: `
+    <h4>I am kv 1</h4>
+    <img src="assets/images/Screenshot from 2023-03-02 12-18-02.png" />
+  `,
+  styles: [],
 })
 export class Kv1Component implements OnInit {
   label: string = 'Kv1 heavy tank';
