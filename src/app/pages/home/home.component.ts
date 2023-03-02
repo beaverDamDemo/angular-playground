9b6702cf677175ac6ede33d5487be111c065ebee
beaverDamDemo <zdenevi@gmail.com>
2023-03-02 13:22:57 +0100

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,8 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: ` <p>home works!</p> `,
-  styles: [],
+  template: `
+    <p>home works!</p>
+    <button nbButton status="warning">
+      <a routerLink="/pages/chainsaw">chainsaw</a>
+    </button>
+    <button nbButton status="danger">
+      <a routerLink="/pages/kamjon">kamjon</a>
+    </button>
+    <button nbButton status="primary">
+      <a routerLink="/pages/letala">letala</a>
+    </button>
+    <button nbButton status="info">
+      <a routerLink="/pages/tanki/heavy-tank">heavt-tank</a>
+    </button>
+  `,
+  styles: [
+    'a {text-decoration: none; color: white;} button { margin: 0.1rem;}',
+  ],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
