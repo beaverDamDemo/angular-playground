ce5c09c55551adf30a47a41ccd601bd4e4e22267
beaverDamDemo <zdenevi@gmail.com>
2023-03-02 10:39:53 +0100

    dodani neki tanki, dela

--- src/app/pages/tanki/tanki/tanki.module.ts
+++ src/app/pages/tanki/tanki/tanki.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeavyTankComponent } from '../heavy-tank/heavy-tank.component';
 import { TankiRoutingModule } from '../tanki-routing.module';
 import { UssrComponent } from '../heavy-tank/ussr/ussr.component';
+import { Kv1Component } from '../heavy-tank/ussr/kv1/kv1.component';
+import { Su85Component } from '../heavy-tank/ussr/su85/su85.component';
 
 @NgModule({
-  declarations: [HeavyTankComponent, UssrComponent],
+  declarations: [
+    HeavyTankComponent,
+    UssrComponent,
+    Kv1Component,
+    Su85Component,
+  ],
   imports: [CommonModule, TankiRoutingModule],
-  exports: [HeavyTankComponent, UssrComponent],
+  exports: [HeavyTankComponent, UssrComponent, Kv1Component, Su85Component],
 })
 export class TankiModule {}
