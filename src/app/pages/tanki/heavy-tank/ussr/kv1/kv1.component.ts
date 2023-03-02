726019ff2ffcddde7430a3040d736dea8da91a1c
beaverDamDemo <zdenevi@gmail.com>
2023-03-02 10:46:21 +0100

    dodan view child

--- src/app/pages/tanki/heavy-tank/ussr/kv1/kv1.component.ts
+++ src/app/pages/tanki/heavy-tank/ussr/kv1/kv1.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-kv1',
   templateUrl: './kv1.component.html',
-  styleUrls: ['./kv1.component.scss']
+  styleUrls: ['./kv1.component.scss'],
 })
 export class Kv1Component implements OnInit {
+  label: string = 'Kv1 heavy tank';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/tanki/heavy-tank/ussr/ussr.component.ts
+++ src/app/pages/tanki/heavy-tank/ussr/ussr.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Kv1Component } from './kv1/kv1.component';
 
 @Component({
   selector: 'app-ussr',
   templateUrl: './ussr.component.html',
-  styleUrls: ['./ussr.component.scss']
+  styleUrls: ['./ussr.component.scss'],
 })
-export class UssrComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class UssrComponent implements OnInit, AfterViewInit {
+  constructor() {}
+  @ViewChild(Kv1Component) kv1_podatki: string;
+  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    if (this.kv1_podatki) {
+      console.log('⚛ ~ kv1_podatki:', this.kv1_podatki['label']);
+    }
   }
-
 }
